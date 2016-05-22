05ddcc4c51be207152b7b586b51f5326994ef91f
Ben Rowe <ben.rowe.83@gmail.com>
2016-05-23 09:07:11 +1000

    Build basic implementation of Facade

---
+++ src/Facades/Config.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Benrowe\Laravel\Config\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Config extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        
+    }
+}
