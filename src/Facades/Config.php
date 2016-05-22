6d39530999083ac17ad09da8a39ff9fd6a926b97
Ben Rowe <ben.rowe.83@gmail.com>
2016-05-23 09:12:21 +1000

    Implement accessor for facade

--- src/Facades/Config.php
+++ src/Facades/Config.php
@@ -4,10 +4,20 @@ namespace Benrowe\Laravel\Config\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Config Facades
+ *
+ * @package Benrowe\Laravel\Config\Facade
+ */
 class Config extends Facade
 {
+    /**
+     * Define service name for facade
+     *
+     * @return string
+     */
     protected static function getFacadeAccessor()
     {
-        
+        return 'config';
     }
 }
