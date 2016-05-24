f13215d1c1f11f72204cd622d8f488ac7ddfc01a
Ben Rowe <ben.rowe.83@gmail.com>
2016-05-24 20:28:19 +1000

    Stub basic test case for Storage mechanism

---
+++ tests/StorageConfigTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use Benrowe\Laravel\Config\Config;
+
+/**
+ * Tests for Persisting Config class
+ */
+class StorageConfigTest extends PHPUnit_Framework_TestCase
+{
+    public function testLoadData()
+    {
+
+    }
+
+    public function testSaveData()
+    {
+
+    }
+
+    public function testClearData()
+    {
+        
+    }
+}
