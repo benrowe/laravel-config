198f39d449eae31ab91ca89e85ba8abb0ef54810
Ben Rowe <ben.rowe.83@gmail.com>
2016-06-06 08:17:26 +1000

    Implement selectStorage mechanism in service provider

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -58,6 +58,36 @@ class ServiceProvider extends BaseServiceProvider
      */
     protected function getConfigPath()
     {
-        return ;
+        return '';
+    }
+
+    protected function selectStorage(Config $config)
+    {
+        $storage = false;
+        if ($config->get('config.storage.enabled')) {
+            $driver = $config->get('config.storage.driver', 'file');
+            switch ($driver) {
+                case 'pdo':
+                    $connection = $config->get('config.storage.connection');
+                    $table = $this->app['db']->getTablePrefix() . 'config';
+                    $pdo = $this->app['db']->connection($connection)->getPdo();
+                    $storage = new Benrowe\Laravel\Config\Storage\Pdo($pdo, $table);
+                    break;
+                case 'redis':
+                    $connection = $config->get('config.storage.connection');
+                    $storage = new RedisStorage($this->app['redis']->connection($connection));
+                    break;
+                case 'custom':
+                    $class = $config->get('config.storage.provider');
+                    $storage = $this->app->make($class);
+                    break;
+                case 'file':
+                default:
+                    $path = $config->get('config.storage.path');
+                    $storage = new Benrowe\Laravel\Config\Storage\File($path);
+                    break;
+            }
+        }
+        return $storage;
     }
 }
