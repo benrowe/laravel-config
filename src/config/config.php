55c792cbba271082f059f7fca3d08c69fa008af5
Ben Rowe <ben.rowe.83@gmail.com>
2016-06-06 08:16:51 +1000

    Update default options in sample config file

--- src/config/config.php
+++ src/config/config.php
@@ -6,7 +6,14 @@ return [
      * Storage Adapters
      * The storage adapter
      */
-    'storage' => Benrowe\Laravel\Config\Adapters\Db::class,
+    'storage' => [
+        'enabled' => true,
+        'driver' => 'file', // redis, file, pdo, custom
+        'path' => storage_path('config'), // For file driver
+        'connection' => null, // leave null for default connection
+        'provider' => '', // instance of StorageInterface for custom driver
+        // Benrowe\Laravel\Config\Adapters\Db::class,
+    ],
 
     /**
      * Anytime a value is created/updated/deleted, do you want to automatically
